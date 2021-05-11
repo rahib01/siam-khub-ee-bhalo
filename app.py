b22a987ede3d7919d53e25166322d4465384f223
rahib01 <83905652+rahib01@users.noreply.github.com>
2021-05-11 15:10:46 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+print("hello world!")
+print("A")
+print("A")
+print("A")
+print("A")
+print("A")
+print("A")
+print(23)
+print(23.768)
+print(True)
+print(False)
+#print("Siam")
+print("###################################################################")
+#Variables
+x = 235732549327549327562149574645764
+y = 36123357425571627172427574125179571
+X = 0
+name = "siam"
+boo1 =  True
+print(name)
+print(boo1)
+print(x)
+print(X)
+print(y)
+print(x,y,X)
+_name = "siam"
+print(_name)
+
+
+#rules of naming  variables
+#1)Don't start with numbers
+#2)don't use special cheracters
+#3)don't use highphen
+
+
+print("#######################################################")
+
+#Data Types
+
+#strings are sorrounded by "" or ''
+#intizers are full number like 0,1,2,3,4,5,6,7,8,9,10,1000,17893 etc
+#floats are number with fractional like 1.24,7.89,10.5 etc
+#booleans are True ans False value
+
+#x=input("please enter labor hours:")
+#abor=int(x)
+#y=input("please enter labor wage rate:")
+#rate=int(y)
+#wage=int(labor*rate)
+#print("wage:BDT ",wage)
+
+#basic operators
+
+print(12+2)
+print(13-11)
+print(14*2)
+print(24/12)
+print(24//12)
+print(23**2)
+print(17%9)
+
+age = 16
+age2 = 17
+print(age - age2)
+print(age + age2)
+print(age * age2)
+print(age / age2)
+print(age // age2)
+print(age ** age2)
+print(age % age2)
+
+#inputs
+
+#name = input("what is your name?")
+#age = int(input("what is your age"))
+#print(name,age)
+
+
+name = input("your name is")
+age = int(input("and your age is"))
+adress = input("Your adress is")
+phone = int(input("we will call you on"))
+mail =input("and we've sent a email to")
+print(name,age,adress,phone,mail)
